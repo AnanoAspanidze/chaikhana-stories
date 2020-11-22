2a76cb0245d91abc8b9193ef4444788599f5220e
iosebi2899 <45330280+iosebi2899@users.noreply.github.com>
2020-11-21 23:52:33 -0800

    მთავარი გვერდი
    მთავარი გვერდის კონსტრუქცია გავაკეთე და გადასვლის ანიმაცია ,width ის ცვალებადობით გავაკეთე თითოეულ ისტორიაზე , მარცხნიდან მარჯვნივ კარგად მიდის ისტორიები ჰოვერზე თუმცა მარცხნიდან მარჯვნივ უჭირს და გასასწორებელი მაქვს

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+
+	$(".story2").hover(function () {
+		$(this).toggleClass("active");
+		$(".story1").toggleClass("active");
+		$(".story2 .black-transparency").toggleClass("active-bl");
+		$(".story1 .black-transparency").toggleClass("active-bl");
+	});
+	$(".story3").hover(function () {
+		$(this).toggleClass("active");
+		$(".story1").toggleClass("active");
+		$(".story3 .black-transparency").toggleClass("active-bl");
+		$(".story1 .black-transparency").toggleClass("active-bl");
+	  });
+	$(".story4").hover(function () {
+		$(this).toggleClass("active");
+		$(".story1").toggleClass("active");
+		$(".story4 .black-transparency").toggleClass("active-bl");
+		$(".story1 .black-transparency").toggleClass("active-bl");
+	  }); 
+	$(".story5").hover(function () {
+		$(this).toggleClass("active");
+		$(".story1").toggleClass("active");
+		$(".story5 .black-transparency").toggleClass("active-bl");
+		$(".story1 .black-transparency").toggleClass("active-bl");
+	  });
+	$(".story6").hover(function () {
+		$(this).toggleClass("active");
+		$(".story1").toggleClass("active");
+		$(".story6 .black-transparency").toggleClass("active-bl");
+		$(".story1 .black-transparency").toggleClass("active-bl");
+	  });
+	$(".story7").hover(function () {
+		$(this).toggleClass("active");
+		$(".story1").toggleClass("active");
+		$(".story7 .black-transparency").toggleClass("active-bl");
+		$(".story1 .black-transparency").toggleClass("active-bl");
+	  });    
