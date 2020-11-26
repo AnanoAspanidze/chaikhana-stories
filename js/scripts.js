0b01a6eda52efe3430e48ed931418d09f1f9adf1
iosebi2899 <45330280+iosebi2899@users.noreply.github.com>
2020-11-26 06:35:27 -0800

    hehe

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@
 		$(".story5").removeClass("active");
 		$(".story6").removeClass("active");
 		$(".story7").removeClass("active");
+		$(this).css('cursor','pointer');
+		setTimeout(function(){ $(".story1").css('cursor','default'); },300);
 	});
 	$(".story2").hover(function () {
 		$(this).addClass("active");
@@ -15,6 +17,8 @@
 		$(".story5").removeClass("active");
 		$(".story6").removeClass("active");
 		$(".story7").removeClass("active");
+		$(this).css('cursor','pointer');
+		setTimeout(function(){ $(".story2").css('cursor','default'); },300);
 	});
 	$(".story3").hover(function () {
 		$(this).addClass("active");
@@ -24,6 +28,8 @@
 		$(".story5").removeClass("active");
 		$(".story6").removeClass("active");
 		$(".story7").removeClass("active");
+		$(this).css('cursor','pointer');
+		setTimeout(function(){ $(".story3").css('cursor','default'); },300);
 	  });
 	$(".story4").hover(function () {
 		$(this).addClass("active");
@@ -33,6 +39,8 @@
 		$(".story5").removeClass("active");
 		$(".story6").removeClass("active");
 		$(".story7").removeClass("active");
+		$(this).css('cursor','pointer');
+		setTimeout(function(){ $(".story4").css('cursor','default'); },300);
 	  });
 	$(".story5").hover(function () {
 		$(this).addClass("active");
@@ -42,6 +50,8 @@
 		$(".story4").removeClass("active");
 		$(".story6").removeClass("active");
 		$(".story7").removeClass("active");
+		$(this).css('cursor','pointer');
+		setTimeout(function(){ $(".story5").css('cursor','default'); },300);
 	  });
 	$(".story6").hover(function () {
 		$(this).addClass("active");
@@ -51,6 +61,8 @@
 		$(".story4").removeClass("active");
 		$(".story5").removeClass("active");
 		$(".story7").removeClass("active");
+		$(this).css('cursor','pointer');
+		setTimeout(function(){ $(".story6").css('cursor','default'); },300);
 	  });
 	$(".story7").hover(function () {
 		$(this).addClass("active");
@@ -60,6 +72,8 @@
 		$(".story4").removeClass("active");
 		$(".story5").removeClass("active");
 		$(".story6").removeClass("active");
+		$(this).css('cursor','pointer');
+		setTimeout(function(){ $(".story7").css('cursor','default'); },300);
 	  });   
 
 	  //slider
