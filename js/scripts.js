b28919540b8c802f409b92013a4c98417f1a200a
iosebi2899 <45330280+iosebi2899@users.noreply.github.com>
2020-12-03 02:26:50 -0800

    story 4 done

--- js/scripts.js
+++ js/scripts.js
@@ -89,19 +89,9 @@
 		fade: true,
 		cssEase: 'linear',
 		arrows: true,
-
-		// ეს 2 სვამს ისრების მაგივრად font-awesome-ის აიქონებს. ასეთ დროს აუცილებელია, რომ შეიცვალოს CSS-იც: 1. დაიმალოს არსებული ისრები (ამაზე სულ ბოლოს დავწერ) 2. დაედოს არსებულ ისარს ზომა და ფერი .slick-arrow i { color: white; font-size: 20px}
-  	// prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-	  // nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
-
-
-		// ეს 2 ღილაკი ადებს ფოტოს ისრების მაგივრად
 		prevArrow:"<button type='button' class='slick-prev pull-left'><img src='img/arrow-left.svg'></button>",
-	  nextArrow:"<button type='button' class='slick-next pull-right'><img src='img/arrow-right.svg'></button>"
+		nextArrow:"<button type='button' class='slick-next pull-right'><img src='img/arrow-right.svg'></button>"
 
-		// CSS-ები:
-		//.slick-next:before  და  .slick-prev:before უნდა იყოს content: none; - ეს გააკეთებს იმას, რომ არსებული ისარი დაიმალება
-		// ფოტოს სტილები: height: 100% არის ძველი ისრის სიმაღლე
 
 
 
